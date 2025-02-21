3f5d6d0cd65cb209b03fabd213697491382a8a11
Daniel Feliciano-Munoz <danielfelicianomunoz@usf.edu>
2025-02-21 15:27:53 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -23,3 +23,30 @@ const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 // runs the methods in the class and logs the outputs in the console
 console.log(emp1.getDetails());
 console.log(emp1.calculateAnnualSalary());
+
+
+// Task 2: Creating a Manager Class (Inheritance & Method Overriding)
+
+
+class Manager extends Employee {  // lines below create a class called manager, that extends the details from Employee
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary); // this line keeps the info from the Employee Class
+        this.teamSize = teamSize;
+    };
+
+    getDetails() { // creates a method inside the class that calls all of the Manager details
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    };
+
+    calculateBonus() { // creates a method inside the class that calculates the annual bonus for the manager
+        return this.calculateAnnualSalary() * .1;
+    };
+
+};
+
+// line below create a manager using the manager class parameters
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+
+// the lines below run the methods used in the manager class and log the outputs in the console
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus());
